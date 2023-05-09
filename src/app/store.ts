e0e6434e3e1ade85731577955c7913d51143d54f
Yelkira <penda.fu@gmail.com>
2023-05-09 21:21:30 +0300

    17 lesson rtk done

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux'
+import {combineReducers} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from '../features/Login/auth-reducer'
+import {configureStore} from "@reduxjs/toolkit";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -14,10 +15,20 @@ const rootReducer = combineReducers({
     auth: authReducer
 })
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware)
+})
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
+export type AppStateType = ReturnType<typeof store.getState>
+export type AppDispatch=typeof store.dispatch
+/*
+export const useAppDispatch:()=>AppDispatch=useDispatch
+export type AppThunkDispatch = ThunkDispatch<AppStateType, unknown, AnyAction>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AnyAction>
+export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector*/
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
